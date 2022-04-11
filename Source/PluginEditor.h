934899c6770fe56e89f48c8444c1aac40cfb1398
ThirdAudio <myownkindofmonster@yahoo.com>
2022-04-11 10:44:23 -0500

    Created repo and added midiSlider
    Slider updates noteOnVel member in parent processor

---
+++ Source/PluginEditor.h
@@ -0,0 +1,36 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class TutorialPluginAudioProcessorEditor  : public juce::AudioProcessorEditor,
+                                            private juce::Slider::Listener
+{
+public:
+    TutorialPluginAudioProcessorEditor (TutorialPluginAudioProcessor&);
+    ~TutorialPluginAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    void sliderValueChanged(juce::Slider* slider) override;
+    
+    TutorialPluginAudioProcessor& audioProcessor;
+
+    juce::Slider midiVolume;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (TutorialPluginAudioProcessorEditor)
+};
